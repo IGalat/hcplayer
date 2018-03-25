256e63859e25fa814ca0eeb674fda6ed48232442
Ilya Galatyuk <galatyuk.ilya@gmail.com>
2018-03-25 23:18:37 +0300

    for test of tracklist front

--- src/main/java/com/jr/structure/model/Song.java
+++ src/main/java/com/jr/structure/model/Song.java
@@ -6,10 +6,18 @@ import java.util.Map;
 
 /**
  * @author Galatyuk Ilya
+ * todo (2) after (1) delete constructor, artistName, trackTitle
  */
 
 @Data
 public class Song {
+    private String artistName;
+    private String trackTitle;
     private String name;
     private Map<Crit, Integer> crits;
+
+    public Song(String artistName, String trackTitle) {
+        this.artistName = artistName;
+        this.trackTitle = trackTitle;
+    }
 }
