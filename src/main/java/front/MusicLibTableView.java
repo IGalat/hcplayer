2bfdc2c15ea76691c367c365087a17713b453771
Ilya Galatyuk <galatyuk.ilya@gmail.com>
2018-08-07 22:04:20 +0300

    songs actually play. wow

--- src/main/java/front/MusicLibTableView.java
+++ src/main/java/front/MusicLibTableView.java
@@ -7,8 +7,12 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,6 +20,8 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class MusicLibTableView implements Initializable {
+
+    MediaPlayer mediaPlayer;
 
     @FXML
     TableView<ID3v2> MusicLibTable = new TableView<>();
@@ -38,6 +44,19 @@ public class MusicLibTableView implements Initializable {
         titleCol.setCellValueFactory(new PropertyValueFactory<ID3v2, String>("title"));
 
         MusicLibTable.setItems(FXCollections.observableArrayList(tags));
+
+        MusicLibTable.setRowFactory(tv -> {
+            TableRow<ID3v2> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY
+                        && event.getClickCount() == 2) {
+
+                    ID3v2 clickedSong = row.getItem();
+                    getSongAndPlay(clickedSong);
+                }
+            });
+            return row;
+        });
     }
 
     @FXML
@@ -45,4 +64,21 @@ public class MusicLibTableView implements Initializable {
         System.out.println("123");
     }
 
+    public void getSongAndPlay(ID3v2 tags) {
+        for (Song song : SongService.getAll()) {
+            if (song.getMp3File() != null && tags.equals(song.getTags())) {
+                Media media = new Media(song.getPath().toUri().toString());
+                if (mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer = null;
+                } else {
+                    mediaPlayer = new MediaPlayer(media);
+                    mediaPlayer.setVolume(0.2);
+                    mediaPlayer.play();
+                }
+                return;
+            }
+        }
+    }
 }
+
