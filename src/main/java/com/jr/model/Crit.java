0e75eb1d07ef2a6b56ca46532bf4ee958e42b1ec
Ilya Galatyuk <galatyuk.ilya@gmail.com>
2018-08-28 23:09:58 +0300

    crit - clone

--- src/main/java/com/jr/model/Crit.java
+++ src/main/java/com/jr/model/Crit.java
@@ -3,6 +3,7 @@ package com.jr.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,11 +12,16 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class Crit {
+public class Crit implements Cloneable {
     private long id;
     private String name;
     private int min;
     private int max;
     private boolean whitelist;
     private List<Crit> children;
+
+    @Override
+    public Object clone() {
+        return new Crit(this.getId(), this.getName(), this.getMin(), this.getMax(), this.isWhitelist(), new ArrayList<>(this.getChildren()));
+    }
 }
